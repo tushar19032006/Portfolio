8f81a5fbd5aae49c32619b823b0944275bf287ad
Tushar Agashe <tusharagashe19032006@gmail.com>
2026-09-25 16:02:57 +0530

    Fix production setting for render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +9,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fpj=j(+1-yl%57fz86&=&4tqh53wsjyb)ny4k7ciy&l1qggpe+'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-fpj=j(+1-yl%57fz86&=&4tqh53wsjyb)ny4k7ciy&l1qggpe+"
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
