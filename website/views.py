5239a05ad6a60158d3728d779aa567a35f2974a4
Tushar Agashe <tusharagashe19032006@gmail.com>
2026-09-26 15:32:09 +0530

    Fix resume download path

--- website/views.py
+++ website/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Project
 from .mongodb import contacts
 
-from django.http import FileResponse
+from django.http import FileResponse, Http404 
+from django.contrib.staticfiles import finders
 from django.conf import settings
 import os
 
@@ -21,9 +22,16 @@ def home(request):
         return redirect("home")
     
 def download_resume(request):
-    resume_path = os.path.join(settings.BASE_DIR, "website", "static", "resume.pdf")
-    return FileResponse(open(resume_path, "rb"), as_attachment=True, filename="Tushar_Resume.pdf")
+    resume_path = finders.find("resume.pdf")
 
+    if not resume_path:
+        raise Http404("Resume file not found")
+
+    return FileResponse(
+        open(resume_path, "rb"),
+        as_attachment=True,
+        filename="Tushar_Resume.pdf"
+    )
     # Show all projects on the homepage
     projects = Project.objects.all()
 
